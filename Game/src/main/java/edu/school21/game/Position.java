b940bf29fb8532a7e322b7f7d045045d97cfcece
ma-akk <gtresa@student.21-school.ru>
2022-07-23 17:56:52 +0300

    position init

--- Game/src/main/java/edu/school21/game/Position.java
+++ Game/src/main/java/edu/school21/game/Position.java
@@ -8,4 +8,40 @@ public class Position {
     private int y;
     private Color color;
     private char design;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public char getDesign() {
+        return design;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setDesign(char design) {
+        this.design = design;
+    }
+
+    public int[] getCoordinates() {
+        return new int[]{x, y};
+    }
 }
