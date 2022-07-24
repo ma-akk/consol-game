8453df5f81d4de587930f6f030df3b6b161184a3
ma-akk <gtresa@student.21-school.ru>
2022-07-24 19:49:41 +0300

    Game main

--- Game/src/main/java/edu/school21/game/Game.java
+++ Game/src/main/java/edu/school21/game/Game.java
@@ -1,7 +1,42 @@
 package edu.school21.game;
 
-public class Game {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Game {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        Logic logicGame = new Logic();
+        logicGame.start(args);
+
+//        System.out.print("\033[H\033[J");
+
+
+        //вариант очистки экрана №1
+//        ConsoleReader r = new ConsoleReader();
+//        while (true)
+//        {
+//            r.println("Good morning");
+//            r.flush();
+//
+//            String input = r.readLine("prompt>");
+//
+//            if ("clear".equals(input))
+//                r.clearScreen();
+//            else if ("exit".equals(input))
+//                return;
+//            else
+//                System.out.println("You typed '" + input + "'.");
+//    }
+
+        //вариант очистки экрана №2
+//        Runtime.getRuntime().exec("clear");
+//        System.out.print("\b") ; - не удаляет предыдущую строчку
+
+        //вариант №3
+//        public static void clearScreen() {
+//            System.out.print("\033[H\033[2J");
+//            System.out.flush();
+//        }
     }
 }
